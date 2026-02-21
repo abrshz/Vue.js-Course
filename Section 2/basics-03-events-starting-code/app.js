fec773d9119c4e4c0a13f08f686f2a160d4de674
abrshz <trusewynur@gmail.com>
2026-02-22 01:52:07 +0300

    Some event listener

--- Section 2/basics-03-events-starting-code/app.js
+++ Section 2/basics-03-events-starting-code/app.js
@@ -2,9 +2,17 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: "",
+      confirmedName: ""
     };
   },
   methods: {
+    submitForm(){
+      alert('Submitted!')
+    },
+    setName(event){
+      this.name = event.target.value
+    },
     increment() {
       return this.counter++;
     },
