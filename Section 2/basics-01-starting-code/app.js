0f3b3179d1d7a8be6bdc2dbad3ffd5b3a75757f9
abrshz <trusewynur@gmail.com>
2026-02-21 21:31:19 +0300

    Data binding and attribute binding

---
+++ Section 2/basics-01-starting-code/app.js
@@ -0,0 +1,22 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      courseGoal: "Finish the course and learn Vue!",
+      vueLink: "https://vue.js.org/" 
+    };
+  },
+  methods: {
+    outputGoal() {
+        const randomNumber = Math.random();
+        if (randomNumber < 0.5 ){
+            return "Learn Vue!"
+        } else {
+            return "Master Vue!"
+        }
+    }
+  }
+});
+
+app.mount("#user-goal");
+
+
